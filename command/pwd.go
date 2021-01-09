9429c94599a165f713aff724736038b4798947bb
Ellery Zhang <allotory@msn.com>
2021-01-09 15:39:26 +0800

    feat: rebuild pwd command

--- command/pwd.go
+++ command/pwd.go
@@ -1,40 +1,37 @@
 package command
 
 import (
-	"bytes"
-	"errors"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 // 查看当前工作目录路径
 func Pwd(cmdStr []string) error {
-
-	// 帮助信息
-	helpStr := `Usage: pwd [-h]
-	Print the name of the current working directory.
-	Options:
-		-h	display this help and exit
-	`
+	pwd := flag.NewFlagSet("pwd", flag.ExitOnError)
+	pwd.Bool("h", false, "display this help")
+	err := pwd.Parse(cmdStr[1:])
 
 	if len(cmdStr) == 1 {
 		//CurrentPath, err := os.Getwd()
 		_, err := fmt.Fprintln(os.Stdout, CurrentPath)
 		return err
-	} else if len(cmdStr) == 2 && cmdStr[1] == "-h" {
-		_, err := fmt.Fprintln(os.Stdout, helpStr)
+	}
+
+	// 帮助信息
+	helpStr := `Usage: pwd [-h]
+	Print the name of the current working directory.
+	Options:
+		-h	display this help and exit`
+
+	pwd.Visit(func(f *flag.Flag) {
+		if f.Name == "h" {
+			_, err = fmt.Fprintln(os.Stdout, helpStr)
+		}
+	})
+	if err != nil {
 		return err
 	}
 
-	// error 信息
-	errStr := "bad command syntax: "
-
-	var buffer bytes.Buffer
-	buffer.WriteString(errStr)
-	buffer.WriteString(strings.Join(cmdStr, " "))
-	buffer.WriteString("\n")
-	buffer.WriteString(helpStr)
-
-	return errors.New(buffer.String())
+	return nil
 }
